a9790004a935317ee5393c3e1e6cffeaae40c872
AryanKRaman <75371016+AryanKRaman@users.noreply.github.com>
2021-02-12 17:58:31 +0530

    Add files via upload
    second

---
+++ Drop.js
@@ -0,0 +1,14 @@
+class Drop{
+  constructor(x,y){
+   super(x,y,50,50)
+  var maxDrops = 100;
+  }
+
+  push();
+  if(var = i=0; i<maxDrops;i++){
+drops.push(new createDrop(random(0,400)), random(0,400)));
+
+  }
+
+
+
